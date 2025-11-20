25064aa9bdd11d8eaa061e47f5447b8450547874
Blackdemond02 <167588991+Blackdemond02@users.noreply.github.com>
2025-11-20 01:14:57 -0400

    Ventana Prestamo

---
+++ SISTEMA_COBRO/Prestamos.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace SISTEMA_COBRO
+{
+    partial class Prestamos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sistemaPrestamosDBDataSet = new SISTEMA_COBRO.SistemaPrestamosDBDataSet();
+            this.prestamosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.prestamosTableAdapter = new SISTEMA_COBRO.SistemaPrestamosDBDataSetTableAdapters.PrestamosTableAdapter();
+            this.prestamoIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clienteIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.montoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tasaInteresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.plazoMesesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaInicioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaFinDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.formaPagoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estadoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sistemaPrestamosDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prestamosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(211, 125);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1116, 613);
+            this.panel3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1078, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.prestamoIDDataGridViewTextBoxColumn,
+            this.clienteIDDataGridViewTextBoxColumn,
+            this.montoDataGridViewTextBoxColumn,
+            this.tasaInteresDataGridViewTextBoxColumn,
+            this.plazoMesesDataGridViewTextBoxColumn,
+            this.fechaInicioDataGridViewTextBoxColumn,
+            this.fechaFinDataGridViewTextBoxColumn,
+            this.formaPagoDataGridViewTextBoxColumn,
+            this.estadoDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.prestamosBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1078, 315);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(943, 15);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 43);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "+ Nuevo Préstamo";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Administre los préstamos del sistema";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gestión de Préstamos";
+            // 
+            // sistemaPrestamosDBDataSet
+            // 
+            this.sistemaPrestamosDBDataSet.DataSetName = "SistemaPrestamosDBDataSet";
+            this.sistemaPrestamosDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // prestamosBindingSource
+            // 
+            this.prestamosBindingSource.DataMember = "Prestamos";
+            this.prestamosBindingSource.DataSource = this.sistemaPrestamosDBDataSet;
+            // 
+            // prestamosTableAdapter
+            // 
+            this.prestamosTableAdapter.ClearBeforeFill = true;
+            // 
+            // prestamoIDDataGridViewTextBoxColumn
+            // 
+            this.prestamoIDDataGridViewTextBoxColumn.DataPropertyName = "PrestamoID";
+            this.prestamoIDDataGridViewTextBoxColumn.HeaderText = "PrestamoID";
+            this.prestamoIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.prestamoIDDataGridViewTextBoxColumn.Name = "prestamoIDDataGridViewTextBoxColumn";
+            this.prestamoIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.prestamoIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // clienteIDDataGridViewTextBoxColumn
+            // 
+            this.clienteIDDataGridViewTextBoxColumn.DataPropertyName = "ClienteID";
+            this.clienteIDDataGridViewTextBoxColumn.HeaderText = "ClienteID";
+            this.clienteIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.clienteIDDataGridViewTextBoxColumn.Name = "clienteIDDataGridViewTextBoxColumn";
+            this.clienteIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // montoDataGridViewTextBoxColumn
+            // 
+            this.montoDataGridViewTextBoxColumn.DataPropertyName = "Monto";
+            this.montoDataGridViewTextBoxColumn.HeaderText = "Monto";
+            this.montoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.montoDataGridViewTextBoxColumn.Name = "montoDataGridViewTextBoxColumn";
+            this.montoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tasaInteresDataGridViewTextBoxColumn
+            // 
+            this.tasaInteresDataGridViewTextBoxColumn.DataPropertyName = "TasaInteres";
+            this.tasaInteresDataGridViewTextBoxColumn.HeaderText = "TasaInteres";
+            this.tasaInteresDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tasaInteresDataGridViewTextBoxColumn.Name = "tasaInteresDataGridViewTextBoxColumn";
+            this.tasaInteresDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // plazoMesesDataGridViewTextBoxColumn
+            // 
+            this.plazoMesesDataGridViewTextBoxColumn.DataPropertyName = "PlazoMeses";
+            this.plazoMesesDataGridViewTextBoxColumn.HeaderText = "PlazoMeses";
+            this.plazoMesesDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.plazoMesesDataGridViewTextBoxColumn.Name = "plazoMesesDataGridViewTextBoxColumn";
+            this.plazoMesesDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // fechaInicioDataGridViewTextBoxColumn
+            // 
+            this.fechaInicioDataGridViewTextBoxColumn.DataPropertyName = "FechaInicio";
+            this.fechaInicioDataGridViewTextBoxColumn.HeaderText = "FechaInicio";
+            this.fechaInicioDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fechaInicioDataGridViewTextBoxColumn.Name = "fechaInicioDataGridViewTextBoxColumn";
+            this.fechaInicioDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // fechaFinDataGridViewTextBoxColumn
+            // 
+            this.fechaFinDataGridViewTextBoxColumn.DataPropertyName = "FechaFin";
+            this.fechaFinDataGridViewTextBoxColumn.HeaderText = "FechaFin";
+            this.fechaFinDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fechaFinDataGridViewTextBoxColumn.Name = "fechaFinDataGridViewTextBoxColumn";
+            this.fechaFinDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // formaPagoDataGridViewTextBoxColumn
+            // 
+            this.formaPagoDataGridViewTextBoxColumn.DataPropertyName = "FormaPago";
+            this.formaPagoDataGridViewTextBoxColumn.HeaderText = "FormaPago";
+            this.formaPagoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.formaPagoDataGridViewTextBoxColumn.Name = "formaPagoDataGridViewTextBoxColumn";
+            this.formaPagoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // estadoDataGridViewTextBoxColumn
+            // 
+            this.estadoDataGridViewTextBoxColumn.DataPropertyName = "Estado";
+            this.estadoDataGridViewTextBoxColumn.HeaderText = "Estado";
+            this.estadoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.estadoDataGridViewTextBoxColumn.Name = "estadoDataGridViewTextBoxColumn";
+            this.estadoDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Prestamos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1695, 976);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Prestamos";
+            this.Text = "Prestamos";
+            this.Load += new System.EventHandler(this.Prestamos_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sistemaPrestamosDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prestamosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private SistemaPrestamosDBDataSet sistemaPrestamosDBDataSet;
+        private System.Windows.Forms.BindingSource prestamosBindingSource;
+        private SistemaPrestamosDBDataSetTableAdapters.PrestamosTableAdapter prestamosTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prestamoIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clienteIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn montoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tasaInteresDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn plazoMesesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaInicioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaFinDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn formaPagoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estadoDataGridViewTextBoxColumn;
+    }
+}
---
+++ SISTEMA_COBRO/Prestamos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SISTEMA_COBRO
+{
+    public partial class Prestamos : Form
+    {
+        public Prestamos()
+        {
+            InitializeComponent();
+        }
+
+        private void Prestamos_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'sistemaPrestamosDBDataSet.Prestamos' Puede moverla o quitarla según sea necesario.
+            this.prestamosTableAdapter.Fill(this.sistemaPrestamosDBDataSet.Prestamos);
+
+        }
+    }
+}
