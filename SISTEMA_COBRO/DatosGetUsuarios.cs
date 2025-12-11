bb5691cea73fbe0b1a045d35f2033fb93d5b0b44
iruel3 <137722027+iruel3@users.noreply.github.com>
2025-12-10 22:15:27 -0400

    dATOS gET

---
+++ SISTEMA_COBRO/DatosGetUsuarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTEMA_COBRO
+{
+    public class DatosGetUsuarios
+    {
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+
+        public DatosGetUsuarios()
+        {
+            Codigo = 0;
+            Nombre = "";
+            Correo = "";
+            Contraseña= "";
+        }
+
+        public DatosGetUsuarios(int PCodigo, string PNombre, string PCorreo, string PContraseña)
+        {
+            Codigo = PCodigo;
+            Nombre = PNombre;
+            Correo = PCorreo;
+            Contraseña = PContraseña;
+        }
+
+    }
+}
